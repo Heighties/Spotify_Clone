20022f126d23a25e8a8fdc178a31b221d65b3196
Heighties <heighties@gmail.com>
2024-04-11 12:56:49 +0200

    Player

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { useDispatch, useSelector } from 'react-redux';
 
 const Discover = () => {
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
     const { data, isFetching, error } = useGetTopChartsQuery();
     const genreTitle = 'pop';
 
@@ -38,6 +41,9 @@ return(
             <SongCard 
             key={song.key}
             song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
             i={i}
             />
         ))}
