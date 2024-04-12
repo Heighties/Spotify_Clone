fc9ee3c2288754de5cf29348371809f537e560d6
Heighties <heighties@gmail.com>
2024-04-12 14:54:31 +0200

    TopPlay

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -76,6 +76,7 @@ const TopPlay = () => {
                 style={{ width: '25%', height: 'auto' }}
                 className="shadow-lg rounded-full animate-slideright"
               >
+                <img src={song?.images?.background} alt="Name" className="rounded-full w-full object-cover" />
                 {/* <Link to={`/artists/${song?.artists[0].adamid}`}>
                   <img src={song?.images?.background} alt="Name" className="rounded-full w-full object-cover" />
                 </Link> */}
