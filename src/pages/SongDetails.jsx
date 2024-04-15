ab3672d28ac8d0053d08ebb893e4dec69c691660
Heighties <heighties@gmail.com>
2024-04-15 11:51:29 +0200

    + SongDetails

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,17 @@
-const SongDetails = () => <div>SongDetails</div>;
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+const SongDetails = () => {
+    const { songid, id: artistId } = useParams();
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+
+    return (
+        <div>SongDetails</div>
+    )
+    
+};
 
 export default SongDetails;
