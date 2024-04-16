05658ad903db5be7387fd7d641922e087c072aac
Heighties <heighties@gmail.com>
2024-04-16 12:39:46 +0200

    + useGetSongDetailsQuery

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
+// import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
+
 
 const SongDetails = () => {
     const { songid } = useParams();
-    // const dispatch = useDispatch();
-    // const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
 
     console.log("songid is " + songid);
 
 
-    return <div>SongDetails</div>
+    return ( 
+        <div className="flex flex-col">
+            {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+            <div className='mb-10'>
+                <h2 className="font-bold text-3xl text-white">Lyrics : </h2>
+                <div className="mt-5">
+
+                </div>
+            </div>
+        </div>
+    )
     
     
 };
