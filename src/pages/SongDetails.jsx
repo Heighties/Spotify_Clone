5fdd07d2c77d09ac0ce07c0ab8b5bc21bc2b97a6
Heighties <heighties@gmail.com>
2024-04-18 17:03:24 +0200

    + <RelatedSong /> in SongDetails

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
-import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from "../redux/services/shazamCore";
 
 const SongDetails = () => {
   const { songid } = useParams();
@@ -11,6 +11,19 @@ const SongDetails = () => {
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data: songData, isFetching: isFetchingSongDetails } =
     useGetSongDetailsQuery({ songid });
+
+    const { data, isFetching: isFetchinRelatedSongs, error } = useGetSongRelatedQuery({ songid });
+    if (isFetchingSongDetails || isFetchinRelatedSongs) return <Loader title="Searching song details" />;
+    if (error) return <Error />
+
+    const handlePauseClick = () => {
+        dispatch(playPause(false));
+      };
+    
+      const handlePlayClick = () => {
+        dispatch(setActiveSong({ song, data, i }));
+        dispatch(playPause(true));
+      };
 
   // Assuming you have a way to get artistId from songData
   const artistId = songData?.artistId; // Replace with your actual logic
@@ -34,6 +47,14 @@ const SongDetails = () => {
           )}
         </div>
       </div>
+      <RelatedSongs 
+        data={data}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePlayClick={handlePlayClick}
+        handlePauseClick={handlePauseClick}
+      />
     </div>
   );
 };
