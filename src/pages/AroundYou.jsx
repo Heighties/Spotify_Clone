466d3321a369fd31609aae363a277e80b2d74321
Heighties <heighties@gmail.com>
2024-04-22 13:23:44 +0200

    + AroundYou

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -4,6 +4,14 @@ import axios from 'axios';
 import {useSelector} from'react-redux';
 import {Error,Loader, SongCard} from '../components';
 
-const AroundYou = () => <div>CountryTracks</div>;
+const AroundYou = () =>{ 
+    const [country, setCountry] = useState('');
+    const [loading, setLoading] = useState(true);
+    const {activeSong,isPlaying} = useSelector((state) => state.player);
+    
+    return(
+        <div>CountryTracks</div>
+    )
+    };
 
 export default CountryTracks;
