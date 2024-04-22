f67df7e7e49e2989d5e8a23a7c542bef4eeebb06
Heighties <heighties@gmail.com>
2024-04-22 13:34:34 +0200

    + AroundYou (working in console)

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -9,14 +9,20 @@ const AroundYou = () =>{
     const [loading, setLoading] = useState(true);
     const {activeSong,isPlaying} = useSelector((state) => state.player);
 
+    console.log(country)
+
     useEffect(() => {
-        //api geo.ipify : 
+        //api key geo.ipify : 
         // at_TcqHdpRSgbK1MpnbcX80XzBBRyTLq
+        axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_TcqHdpRSgbK1MpnbcX80XzBBRyTLq`)
+        .then((res) => setCountry(res?.data?.location?.country))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
     }, [country]);
     
     return(
-        <div>CountryTracks</div>
+        <div/>
     )
     };
 
-export default CountryTracks;
+export default AroundYou;
