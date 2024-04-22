7150b1d97dc52fe1862c5cc2eee2287aa46762ab
Heighties <heighties@gmail.com>
2024-04-22 13:44:07 +0200

    import correction AroundYou

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -3,11 +3,13 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import {useSelector} from'react-redux';
 import {Error,Loader, SongCard} from '../components';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
 const AroundYou = () =>{ 
     const [country, setCountry] = useState('');
     const [loading, setLoading] = useState(true);
     const {activeSong,isPlaying} = useSelector((state) => state.player);
+    const {data,isFetching,error} = useGetSongsQuery(country);
 
     console.log(country)
 
@@ -19,9 +21,15 @@ const AroundYou = () =>{
         .catch((err) => console.log(err))
         .finally(() => setLoading(false));
     }, [country]);
+
+    if(isFetching && loading) return <Loader title="Loading songs around you" />
+
+    if(error && country) return <Error />
     
     return(
-        <div/>
+        <div className='flex flex-col'>
+            <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around you</h2>
+        </div>
     )
     };
 
