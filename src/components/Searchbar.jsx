6ec4bbb590bf7d37c1644fdfb9a363fe02a26abe
Heighties <heighties@gmail.com>
2024-04-25 16:29:55 +0200

    + SearchBar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,13 @@
 const Searchbar = () => (
-  <div>Loader</div>
+  <form
+    autoComplete="off"
+    className="p-2 text-gray-400 focus-within:text-gray-600"
+  >
+    <label htmlFor="search-fiels" className="sr-only">
+      Search all songs
+    </label>
+    <div className="flex flex-row justify-start items-center"></div>
+  </form>
 );
 
 export default Searchbar;
